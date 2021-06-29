48ff8d2ff6f943d2b911145279e49b2d2bd2d616
Emerson Ferreira <emecatarino@yahoo.com.br>
2021-06-29 00:36:13 -0300

    Migracao inicial, definicao do model da aplicacao de enquete e diagrama de classes UML

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(
+        'questão',
+        max_length=200,
+    )
+    pub_date = models.DateTimeField(
+        'data de publicação',
+    )
+
+
+class Choide(models.Model):
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        verbose_name='Questão',
+        related_name='choices'  # eu coloquei
+    )
+    choice_text = models.CharField(
+        'texto da escolha',
+        max_length=200,
+    )
+    votes = models.IntegerField(
+        'votos',
+        default=0
+    )
