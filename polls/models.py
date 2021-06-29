aac6eba0a5f75867868cebd97ad0c50355eb599f
Emerson Ferreira <emecatarino@yahoo.com.br>
2021-06-29 02:03:17 -0300

    Metodos de representacao do model e personalizado, insercao, consulta, alteracao e delecao de registros pelo terminal do Django

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -10,13 +13,19 @@ class Question(models.Model):
         'data de publicação',
     )
 
+    def __str__(self) -> str:
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(
         Question,
         on_delete=models.CASCADE,
         verbose_name='Questão',
-        related_name='choices'  # eu coloquei
+        related_name='choices'  # eu coloquei - o padrão é choice_set
     )
     choice_text = models.CharField(
         'texto da escolha',
@@ -26,3 +35,6 @@ class Choice(models.Model):
         'votos',
         default=0
     )
+
+    def __str__(self) -> str:
+        return self.choice_text
